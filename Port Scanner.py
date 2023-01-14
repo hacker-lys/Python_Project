06fc641ad2cf755f828a26c7221f2ffdbf656f18
▇ Red Eye <104682865+hacker-lys@users.noreply.github.com>
2023-01-14 14:34:51 +0800

    Add files via upload

---
+++ Port Scanner.py
@@ -0,0 +1,20 @@
+import socket 
+import argparse
+import sys
+import time
+parse = argparse.ArgumentParser()
+parser.add_argument("host") 
+args = parser.parse_args()
+start = tiem.time()
+try:
+    for port in range(1,65536):
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((args.host,port))
+        if result == 0 :
+            print("Port:{} Open".format(port))
+        sock.close()
+except KeyboardInterrupt:
+    sys.exit()
+end = time.time()
+print(f"Scanning completed in:{end-start:.3f}s")
